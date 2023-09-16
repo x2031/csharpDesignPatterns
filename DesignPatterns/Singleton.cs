8b09edabf62955fae0bceeb639535d4b3778fe86
X203.1 <334993067@qq.com>
2023-09-16 22:08:49 +0800

    添加项目文件。

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton() { }
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
